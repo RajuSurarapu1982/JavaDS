902d14e1c2d5d2bb7523385c090525846cccdb58
Manvitha <Manvitha@10.0.0.4>
2015-12-13 12:34:08 -0800

    print each level using LinkedList and Queue

--- DS/src/com/raju/ds/BinaryTreeLevelOrderTraversal.java
+++ DS/src/com/raju/ds/BinaryTreeLevelOrderTraversal.java
@@ -1,4 +1,8 @@
 package com.raju.ds;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 //Given a Binary Tree, Print each level in separate line
 public class BinaryTreeLevelOrderTraversal {
 
@@ -12,7 +16,12 @@ public class BinaryTreeLevelOrderTraversal {
 		root.right.left = new Node2(6);
 		root.right.right = new Node2(7);
 		
+		// print each level using for loop
 		levelOrderTraversal(root);
+		
+		// print each level using LinkedList 
+		
+		printEachLevelUsingQueue(root);
 	}
 	
 	public static void levelOrderTraversal(Node2 root){
@@ -38,6 +47,38 @@ public class BinaryTreeLevelOrderTraversal {
 			printEachLevel(root.left, height -1);
 			printEachLevel(root.right, height -1);
 		}	
+	}
+	
+	/*Bet­ter Solution : 
+		◾Cre­ate a ArrayList of Linked List Nodes
+		◾Do the level order tra­ver­sal using queue(Breadth First Search). 
+		◾For get­ting all the nodes at each level, before you take out a node from queue, store the size of the queue in a vari­able, say you call it as levelNodes.
+		◾Now while levelNodes>0, take out the nodes and print it and add their chil­dren into the queue.
+		◾After this while loop put a line break.
+      */
+	
+	public static void printEachLevelUsingQueue(Node2 root){
+		int levelNodes = 0;
+		Queue<Node2> queue = new LinkedList<Node2>();
+		if(root != null){
+			queue.add(root);
+		}
+		
+		while(!queue.isEmpty()){
+			levelNodes = queue.size();
+			
+			while(levelNodes > 0){
+				
+				Node2 node = queue.remove();
+				System.out.print(node.data);
+				
+				if(node.left != null) queue.add(node.left);
+				if(node.right != null) queue.add(node.right);
+				
+				levelNodes --;
+			}
+			System.out.println("");
+		}
 	}
 }
 
